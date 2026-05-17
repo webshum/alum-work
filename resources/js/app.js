8ae13bd406fdf2a2e487f8749aee44e91c8ca81e
webshum <webshumweb@gmail.com>
2026-05-17 16:21:39 +0300

    add swiper

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue/dist/vue.esm-bundler';
+import { register } from 'swiper/element/bundle';
 import Home from './components/Home.vue';
+
+register();
 
 // createApp(Home).mount('#app');
--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,13 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Front Page</h1>
+    <h1>Front Page dfsdfds</h1>
+
+    <div class="flex items-center jusityf-between">
+        <div>
+            LEFT
+        </div>
+
+        <div>
+            RIGHT
+        </div>
+    </div>
 
     <swiper-container>
-  <swiper-slide>Slide 1</swiper-slide>
-  <swiper-slide>Slide 2</swiper-slide>
-  <swiper-slide>Slide 3</swiper-slide>
-  ...
-</swiper-container>
-
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+    </swiper-container>
 @endsection
