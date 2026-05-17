33c1265caa4a783bc1bbd317aab3cf15829180e8
webshum <webshumweb@gmail.com>
2026-05-17 15:39:11 +0300

    update header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,1 +1,9 @@
-hello
+<header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contacts</a></li>
+        </ul>
+    </nav>
+</header>
