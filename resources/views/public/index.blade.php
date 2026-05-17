2ec0cfaf69d42d4100eb0d5eb36b17d9c4f02070
ilnitskiymichail-dev <ilnitskiymichail@gmail.com>
2026-05-17 15:15:03 +0200

    slider

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -2,4 +2,12 @@
 
 @section('content')
     <h1>Front Page</h1>
+
+    <swiper-container>
+  <swiper-slide>Slide 1</swiper-slide>
+  <swiper-slide>Slide 2</swiper-slide>
+  <swiper-slide>Slide 3</swiper-slide>
+  ...
+</swiper-container>
+
 @endsection
