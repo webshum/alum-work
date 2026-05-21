74550b5fec4571079b25fc0e6fce5fc151964532
ilnitskiymichail-dev <ilnitskiymichail@gmail.com>
2026-05-21 18:53:35 +0200

    phoneWork

---
+++ resources/views/components/promo.blade.php
@@ -0,0 +1,19 @@
+ <section class="promo">
+    <div class="center">
+        <h2 class="title">Profitieren Sie jetzt von unseren Aktionen</h2>
+        
+        <div class="wrap">
+            <a href="#" class="item">
+             <img src="{{ asset('images/promo-1.png') }}" alt="10% Rabatt">
+            </a>
+
+            <a href="#" class="item">
+                <img src="{{ asset('images/promo-2.png') }}" alt="Kontakt aufnehmen">
+            </a>
+
+            <a href="#" class="item">
+                <img src="{{ asset('images/promo-3.png') }}" alt="LED-Lampen-Set">
+            </a>
+        </div>
+    </div>
+</section>
