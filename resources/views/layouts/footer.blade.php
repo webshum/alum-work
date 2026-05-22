0c98e19af44bfb75bba5cdf32a408d20d6d3b95b
ilnitskiymichail-dev <ilnitskiymichail@gmail.com>
2026-05-22 15:24:46 +0200

    pagePhone

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,89 @@
-<div class="footer center">
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate eligendi maxime veniam cumque iste. Explicabo eius nemo quod nobis ipsam dolorem a tempora, molestias exercitationem excepturi provident, eaque sunt possimus!
-</div>
+
+<footer class="footer">
+  <div class="center">
+    <div class="footer-grid">
+      
+      <!-- Колонка 1: Лого + Контакти + Соцмережі -->
+      <div class="footer-col footer-col--info">
+        <a href="/" class="footer-logo">
+          <img src="{{ asset('images/logo-white.svg') }}" alt="ALUMWORK">
+        </a>
+        
+        <ul class="footer-contacts">
+          <li>
+            <svg>...</svg> <!-- іконка телефону -->
+            <a href="tel:079319589177">07931 / 958 91 77</a>
+          </li>
+          <li>
+            <svg>...</svg> <!-- іконка телефону -->
+            <a href="tel:016751502254">01675 / 150 22 54</a>
+          </li>
+          <li>
+            <svg>...</svg> <!-- іконка пошти -->
+            <a href="mailto:info@alumwork.de">info@alumwork.de</a>
+          </li>
+          <li>
+            <svg>...</svg> <!-- іконка локації -->
+            <span>Nussacker 1 | 97999<br>Igersheim</span>
+          </li>
+        </ul>
+
+        <div class="footer-socials">
+          <a href="#" aria-label="Instagram"><svg>...</svg></a>
+          <a href="#" aria-label="TikTok"><svg>...</svg></a>
+          <a href="#" aria-label="Facebook"><svg>...</svg></a>
+        </div>
+      </div>
+
+      <!-- Колонка 2: Service -->
+      <div class="footer-col">
+        <h4 class="footer-title">Service</h4>
+        <ul class="footer-menu">
+          <li><a href="#">Montage</a></li>
+          <li><a href="#">Lieferung</a></li>
+          <li><a href="#">Beratung</a></li>
+          <li><a href="#">Garantie</a></li>
+          <li><a href="#">Häufig gestellte Fragen</a></li>
+        </ul>
+      </div>
+
+      <!-- Колонка 3: Unsere Produkte -->
+      <div class="footer-col">
+        <h4 class="footer-title">Unsere Produkte</h4>
+        <ul class="footer-menu">
+          <li><a href="#">Terrassenüberdachung</a></li>
+          <li><a href="#">Balkonüberdachung</a></li>
+          <li><a href="#">Carport</a></li>
+          <li><a href="#">Vordach</a></li>
+          <li><a href="#">Markise</a></li>
+          <li><a href="#">Beschattung</a></li>
+          <li><a href="#">Raffstore</a></li>
+          <li><a href="#">Geländer</a></li>
+        </ul>
+      </div>
+
+      <!-- Колонка 4: Rechtliches -->
+      <div class="footer-col">
+        <h4 class="footer-title">Rechtliches</h4>
+        <ul class="footer-menu">
+          <li><a href="#">AGB</a></li>
+          <li><a href="#">Datenschutz</a></li>
+          <li><a href="#">Impressum</a></li>
+          <li><a href="#">Vorteile</a></li>
+        </ul>
+      </div>
+
+    </div>
+
+    <!-- Нижня лінія -->
+    <div class="footer-bottom">
+      <p class="footer-copy">© ALUMWORK - 2025 Alle Rechte vorbehalten</p>
+      <ul class="footer-bottom-menu">
+        <li><a href="#">AGB</a></li>
+        <li><a href="#">Datenschutzerklärung</a></li>
+        <li><a href="#">Impressum</a></li>
+      </ul>
+    </div>
+
+  </div>
+</footer>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,14 +1,42 @@
-<header class="header">
-    <div class="center">
-        <a href="/" class="header-logo">
-            <img src="{{ asset('images/logo.svg') }}" alt="">
-        </a>
+<header class="header"><header class="header">
+  <div class="center">
+    <a href="/" class="header-logo">
+      <img src="{{ asset('images/logo.svg') }}" alt="ALUMWORK">
+    </a>
 
-        <nav class="header-nav">
-            <a href="#">Produkte</a>
-            <a href="#">Leistungen</a>
-            <a href="#">Über Uns</a>
-            <a href="#">Kontakt</a>
-        </nav>
-    </div>
+    <nav class="header-nav">
+      <ul class="nav-list">
+        <li class="nav-item has-dropdown">
+          <a href="#" class="nav-link">Produkte</a>
+          <ul class="dropdown">
+            <li><a href="#">Terrassenüberdachung</a></li>
+            <li><a href="#">Sommergarten</a></li>
+            <li><a href="#">Carport</a></li>
+            <li><a href="#">Eingangsüberdachung</a></li>
+            <li><a href="#">Balkonüberdachung</a></li>
+            <li><a href="#">Sonnenschutz</a></li>
+            <li><a href="#">Glasschiebewand</a></li>
+            <li><a href="#">Geländer</a></li>
+          </ul>
+        </li>
+        <li class="nav-item"><a href="#" class="nav-link">Leistungen</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Über Uns</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Kontakt</a></li>
+      </ul>
+    </nav>
+  </div>
 </header>
+            <div class="center">
+                            <a href="/" class="header-logo">
+                                                        <img src="{{ asset('images/logo.svg') }}" alt="">
+                                                                                            </a>
+
+                                                                                                                                        <nav class="header-nav">
+                                                                                                                                                                                                <a href="#">Produkte</a>
+                                                                                                                                                                                                                                                                    <a href="#">Leistungen</a>
+                                                                                                                                                                                                                                                                                                                                                    <a href="#">Über Uns</a>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                <a href="#">Kontakt</a>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    </nav>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            </div>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    </header>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
