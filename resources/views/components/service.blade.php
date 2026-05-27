b2d2cc24ac94864eeae6c9d8e4a32e022a353a78
webshum <webshumweb@gmail.com>
2026-05-27 21:22:58 +0300

    refactoring service

---
+++ resources/views/components/service.blade.php
@@ -0,0 +1,23 @@
+<section class="service">
+    <div class="center">
+        <ul>
+            <li>
+                <h3>Kundendienst</h3>
+                <p>Unser Kundendienst ist 365 Tage im Jahr erreichbar</p>
+            </li>
+
+            <li>
+                <h3>Sicheres Bestellen</h3>
+                <p>Bestellen Sie sicher und einfach</p>
+            </li>
+
+            <li>
+                <h3>Service & unsere Produkte</h3>
+            </li>
+        </ul>
+
+        <div class="manager">
+            <img src="{{ asset('images/service-1.png') }}" alt="Manager">
+        </div>
+    </div>
+</section>
--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -38,29 +38,7 @@
 </section>
 @endif
 
-<section class="service">
-        <div class="center">
-            <div class="wrap">
-                <div class="service-item">
-                    <h3>Kundendienst</h3>
-                    <p>Unser Kundendienst ist 365 Tage im Jahr erreichbar</p>
-                </div>
-
-                <div class="service-item">
-                    <h3>Sicheres Bestellen</h3>
-                    <p>Bestellen Sie sicher und einfach</p>
-                </div>
-
-                <div class="service-item">
-                    <h3>Service & unsere Produkte</h3>
-                </div>
-            </div>
-
-            <div class="service-manager">
-                <img src="{{ asset('images/service-1.png') }}" alt="Manager">
-            </div>
-        </div>
-</section>
+<x-service/>
 
  <section class="promo">
     <div class="center">
